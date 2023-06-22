cd51d5ffcbb433f88770ba6181d52b93588895cf
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-06-23 03:04:48 +0800

    chore: find build-tools version automatically
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,3 +1,5 @@
+import java.io.File
+import java.util.Properties
 import org.gradle.api.JavaVersion
 
 object Version {
@@ -10,4 +12,28 @@ object Version {
 
     @JvmStatic
     val isStable = true
+
+    fun findBuildToolsVersion(): String {
+        val defaultBuildToolsVersion = "33.0.0"
+        return File(System.getenv("ANDROID_HOME"), "build-tools").listFiles()?.filter { it.isDirectory }?.maxOfOrNull { it.name }?.also { println("Using build tools version $it") }
+            ?: defaultBuildToolsVersion
+    }
+
+    fun findNdkVersion(): String {
+        val defaultNdkVersion = "23.2.8568313"
+        val ndkBundle = File(System.getenv("ANDROID_HOME"), "ndk-bundle")
+        if (ndkBundle.exists()) {
+            val sourceProperties = File(ndkBundle, "source.properties")
+            if (sourceProperties.exists()) {
+                val properties = Properties()
+                sourceProperties.inputStream().use {
+                    properties.load(it)
+                }
+                return properties.getProperty("Pkg.Revision").also { if (it != null) println("Using ndk version $it") }
+                    ?: defaultNdkVersion
+            }
+        }
+        return File(System.getenv("ANDROID_HOME"), "ndk").listFiles()?.filter { it.isDirectory }?.maxOfOrNull { it.name }.also { if (it != null) println("Using ndk version $it") }
+            ?: defaultNdkVersion
+    }
 }
