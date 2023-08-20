icewithcola/Kaguram

fix: settings link
Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

TMessagesProj/src/main/java/xyz/nextalone/nnngram/activity/PasscodeSettingActivity.java
@@ -310,7 +310,7 @@ public class PasscodeSettingActivity extends BaseActivity {
                     } else if (position == panicCode2Row) {
                         cell.setText(LocaleController.getString(R.string.PasscodePanicCodeAbout));
                     } else if (position == showInSettings2Row) {
-                        var link = String.format(Locale.ENGLISH, "https://t.me/nnnsetting/%s", PasscodeHelper.getSettingsKey());
+                        var link = String.format(Locale.ENGLISH, "https://t.me/nnnsettings/%s", PasscodeHelper.getSettingsKey());
                         var stringBuilder = new SpannableStringBuilder(AndroidUtilities.replaceTags(LocaleController.getString(R.string.PasscodeShowInSettingsAbout)));
                         stringBuilder.append("\n").append(link);
                         stringBuilder.setSpan(new URLSpanNoUnderline(null) {

cxlg66you/Nnngram1

TMessagesProj/src/main/java/xyz/nextalone/nnngram/activity/PasscodeSettingActivity.java
@@ -308,7 +308,7 @@ public class PasscodeSettingActivity extends BaseActivity {
                             cell.setBackground(Theme.getThemedDrawable(mContext, R.drawable.greydivider_bottom, Theme.key_windowBackgroundGrayShadow));
                         }
                     } else if (position == showInSettings2Row) {
-                        var link = String.format(Locale.ENGLISH, "https://t.me/nnnsetting/%s", PasscodeHelper.getSettingsKey());
+                        var link = String.format(Locale.ENGLISH, "https://t.me/nasettings/%s", PasscodeHelper.getSettingsKey());
                         var stringBuilder = new SpannableStringBuilder(AndroidUtilities.replaceTags(LocaleController.getString("PasscodeShowInSettingsAbout", R.string.PasscodeShowInSettingsAbout)));
                         stringBuilder.append("\n").append(link);
                         stringBuilder.setSpan(new URLSpanNoUnderline(null) {