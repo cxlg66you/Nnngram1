8447ce8964653c112995f0b7c8f4cb2ec1a540f9
cxlg66you <cxlg66you@gmail.com>
2023-08-21 20:33:06 +0800

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -38,7 +38,7 @@ public class BuildVars {
 
 
     // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
-    public static boolean IS_BILLING_UNAVAILABLE = false;
+    public static boolean IS_BILLING_UNAVAILABLE = true;
 
     static {
         if (ApplicationLoader.applicationContext != null) {
